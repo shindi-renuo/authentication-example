ecbe346cbc24cbde667de08516de8fc726c3fe2d
Sami Hindi <sami@samihindi.com>
2025-04-04 15:19:21 +0200

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_04_112728) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_04_131027) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
